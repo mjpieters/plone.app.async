389d2dea7d228d166b857ed591cedb69a3d8cb2e
stefan <stefan@dd099063-dbd4-0310-9226-844929cde8ce>
2010-08-17 07:31:07 +0000

    Cosmetics.
    git-svn-id: https://svn.jarn.com/customers/elkjop/plone.app.async/trunk@38290 dd099063-dbd4-0310-9226-844929cde8ce

--- src/plone/app/async/tests/test_doctests.py
+++ src/plone/app/async/tests/test_doctests.py
@@ -7,20 +7,18 @@ optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
 
-def modSetUp(test):
-   module.setUp(test, 'plone.app.async.tests.doctest_test')
+def setUp(test):
+    module.setUp(test, 'plone.app.async.tests.fakemodule')
 
 
-def modTearDown(test):
-   import transaction
-   transaction.abort()
-   module.tearDown(test)
+def tearDown(test):
+    module.tearDown(test)
 
 
 def test_suite():
     suite = ZopeTestCase.FunctionalDocFileSuite(
             'README.txt', package='plone.app.async',
             test_class=FunctionalAsyncTestCase,
-            optionflags=optionflags,
-            setUp=modSetUp, tearDown=modTearDown)
+            setUp=setUp, tearDown=tearDown,
+            optionflags=optionflags)
     return suite

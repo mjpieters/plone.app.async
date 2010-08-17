e923211716fb7da05a681c3d69e5e577025e58ad
ggozad <ggozad@dd099063-dbd4-0310-9226-844929cde8ce>
2010-08-17 13:07:31 +0000

    Check that db is the same in the dispatcher. Refs #579
    git-svn-id: https://svn.jarn.com/customers/elkjop/plone.app.async/trunk@38310 dd099063-dbd4-0310-9226-844929cde8ce

--- src/plone/app/async/tests/test_setup.py
+++ src/plone/app/async/tests/test_setup.py
@@ -1,21 +1,37 @@
-from zope.component import queryUtility, getUtility
+import transaction
+from zope.component import queryUtility
 from zc.async import dispatcher
-from plone.app.async.interfaces import IAsyncDatabase, IAsyncService
+from zc.async.testing import wait_for_result
+from plone.app.async.interfaces import IAsyncDatabase
 from plone.app.async.tests.base import AsyncTestCase
+
+
+def dbUsed(context):
+    return str(context._p_jar.db())
 
 
 class TestSetup(AsyncTestCase):
     """
     """
 
-    def test_setup(self):
+    def test_async_db_present(self):
         """
         """
         self.failUnless(queryUtility(IAsyncDatabase) is not None)
-        self.failUnless(dispatcher.get() is not None)
-        async = getUtility(IAsyncService)
-        self.failUnless(async.getQueues() is not None)
 
+    def test_dispatcher_present(self):
+        self.failUnless(dispatcher.get() is not None)
+
+    def test_queues_present(self):
+        self.failUnless(self.async.getQueues() is not None)
+
+    def test_same_db(self):
+        """ Tests whethere the dispatcher sees the same db.
+        """
+        job = self.async.queueJob(dbUsed, self.folder)
+        transaction.commit()
+        wait_for_result(job)
+        self.assertEqual(job.result, str(self.app._p_jar.db()))
 
 def test_suite():
     from unittest import TestSuite, makeSuite

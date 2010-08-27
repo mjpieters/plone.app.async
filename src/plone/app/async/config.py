1f1d7ba7af49e994cb6dc780858f2bd649ab67b1
stefan <stefan@dd099063-dbd4-0310-9226-844929cde8ce>
2010-08-27 14:26:01 +0000

    Support single-db and multi-db setups.
    git-svn-id: https://svn.jarn.com/customers/elkjop/plone.app.async/trunk@38648 dd099063-dbd4-0310-9226-844929cde8ce

--- src/plone/app/async/config.py
+++ src/plone/app/async/config.py
@@ -1,24 +1,24 @@
 import os
+import logging
 import Zope2
 from ZODB.interfaces import IDatabase
 from Zope2.App import startup
 from App.config import getConfiguration
 from AccessControl.SecurityManagement import noSecurityManager
-
 from zope import interface, component
 from zope.app.appsetup.interfaces import DatabaseOpened
-
 import zc.monitor
 import zc.async.dispatcher
 from zc.async import subscribers
 from plone.app.async.interfaces import IInitAsync, IAsyncDatabase
-from plone.app.async import logger
+
+logger = logging.getLogger('plone.app.async')
 
 
 class InitBase(object):
     interface.implements(IInitAsync)
 
-    dbEvents = ()
+    db_name = None
 
     def init(self):
         component.provideUtility(Zope2.DB, IDatabase)
@@ -28,35 +28,40 @@ class InitBase(object):
             db = configuration.dbtab.getDatabase(name=name)
             component.provideUtility(db, IDatabase, name=name)
 
-        db = configuration.dbtab.getDatabase(name='main')
+        db = configuration.dbtab.getDatabase(name=self.db_name)
         component.provideUtility(db, IAsyncDatabase)
 
         ev = DatabaseOpened(db)
         subscribers.queue_installer(ev)
-
-        for handler in self.dbEvents:
-            handler(ev)
+        subscribers.threaded_dispatcher_installer.poll_interval = 2
+        subscribers.threaded_dispatcher_installer(ev)
 
         config = configuration.product_config.get('zc.z3monitor')
         if config and 'port' in config:
-            logger.info('Starting zc.monitor service on %s port',
+            logger.info('Starting zc.monitor service on port %s',
                         config['port'])
             zc.monitor.start(int(config['port']))
 
 
-subscribers.threaded_dispatcher_installer.poll_interval = 2
+class InitSingleDB(InitBase):
+    db_name = 'main'
 
 
-class InitSingleDBAsync(InitBase):
-    dbEvents = (subscribers.threaded_dispatcher_installer,)
+class InitMultiDB(InitBase):
+    db_name = 'async'
 
 
 def init_zasync():
     noSecurityManager()
 
-    component.getUtility(IInitAsync).init()
+    initializer = component.queryUtility(IInitAsync)
+    if initializer is not None:
+        initializer.init()
+
     startup.noSecurityManager = noSecurityManager
 
+
+# TODO: Subscribe to IProcessStarting instead (Zope >= 2.11)
 startup.noSecurityManager = init_zasync
 
 
@@ -71,14 +76,11 @@ if os.name == 'nt':
     try:
         from Signals.WinSignalHandler import SignalHandler
     except ImportError:
-        pass
         SignalHandler = None
 else:
     from Signals.SignalHandler import SignalHandler
 
-
 if SignalHandler is not None:
     from signal import SIGTERM, SIGINT
-
     SignalHandler.registerHandler(SIGINT, shutdownDispatcher)
     SignalHandler.registerHandler(SIGTERM, shutdownDispatcher)

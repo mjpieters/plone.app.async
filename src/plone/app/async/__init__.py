4d6994253ba58c92acd070690c54e1a27551bf1c
stefan <stefan@dd099063-dbd4-0310-9226-844929cde8ce>
2010-08-27 14:20:23 +0000

    Remove unused code.
    git-svn-id: https://svn.jarn.com/customers/elkjop/plone.app.async/trunk@38646 dd099063-dbd4-0310-9226-844929cde8ce

--- src/plone/app/async/__init__.py
+++ src/plone/app/async/__init__.py
@@ -1,9 +1,1 @@
-import logging
-from Products.CMFCore.interfaces import ISiteRoot
-from zc.async.interfaces import KEY
-from zope.component import getUtility
-
-logger = logging.getLogger('plone.app.async')
-    
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+# plone.app.async
